c71210f1fbcad15ddbe5401b193d214bb8f8309b
michellelle <roxytoeshoe@gmail.com>
2016-12-21 23:21:51 +0000

    added files for devise

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,16 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    
+    def create
+        super do |resource|
+            if params[:plan]
+                resource.plan_id = params[:plan]
+                if resource.plan_id == 1
+                    resource.save
+                else
+                    resource.save
+                end
+            end
+        end
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models, controllers: { registrations: 'users/registrations' }
   resources :contacts
     get '/about' => 'pages#about'
     root 'pages#home'
