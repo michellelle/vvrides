7a45121df3ac333d51768da81cd57009f7bcec53
michellelle <roxytoeshoe@gmail.com>
2016-12-26 07:49:24 +0000

    modified devise folder for user registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     
     def create
-        super do |resource|
-            if params[:plan]
-                resource.plan_id = params[:plan]
-                if resource.plan_id == 1
-                    resource.save
-                else
-                    resource.save
-                end
-            end
-        end
     end
 
 end
