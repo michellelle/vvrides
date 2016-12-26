eb616fffc077949a070113292fc0a56ef9c51625
michellelle <roxytoeshoe@gmail.com>
2016-12-26 06:27:37 +0000

    Added mailer and view, updated home page glyph css

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'upgrade2mode@gmail.com'
+    default to: 'michelledesign789e@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
